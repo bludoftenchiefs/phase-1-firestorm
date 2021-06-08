6eee1db7466a259e9901d799ebc89bdf0a4599ed
rilith <bludoftenchiefs@protonmail.com>
2021-06-08 02:33:13 -0500

    updated code. event listeners need work

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-stuff
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+const nerdThing = document.getElementById('nerdthing');
+const nerdInput = document.getElementById('nerd-input');
+
+nerdThing.addEventListener('click', function() {
+    let btn = document.createElement('li');
+    btn.innerHTML = nerdInput.value;
+    document.body.appendChild(btn);
+    nerdInput.value = '';
+     })
+})
+
+document.getElementById('hint').addEventListener('mouseover', mouseOver);
+document.getElementById('hint').addEventListener('mouseout', mouseOut);
+
+function mouseOver () {
+    document.getElementById('hint').innerHTML = 'there are more than you think';
+}
+function mouseOut(){
+  document.getElementById('hint').innerHTML = '';
+}
+
+const answers = document.getElementById('answers');
+answers.addEventListener('click', function() {
+  alert('Ronnie Raymond/Martin Stein, Mikhail Arkadan/Jason Thomas, Ronnie Raymond/Jason Thomas, Ronnie/Stein/Arkadan, Jason Thomas/Martin Stein, Jason Thomas/Firehawk, Fire Elemental');
+})
