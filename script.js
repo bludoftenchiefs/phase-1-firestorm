42798396dec19e209e34c35c133377ae55d47ff9
rilith <bludoftenchiefs@protonmail.com>
2021-06-15 20:30:57 -0500

    final changes

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-const nerdThing = document.getElementById('nerdthing');
-const nerdInput = document.getElementById('nerd-input');
 
-nerdThing.addEventListener('click', function() {
-    let btn = document.createElement('li');
-    btn.innerHTML = nerdInput.value;
-    document.body.appendChild(btn);
-    nerdInput.value = '';
-     })
-})
+  function nerdInput() {
+    return fetch("https://superheroapi.com/api.php/1476336809372383/261/biography")
+    .then(function(response) {
+        return response.json();
+      })
+  
+    .then (function(response) {
+     return document.getElementById('nerd-input').innerHTML = response['first-appearance']; 
+    })
+  }
+  nerdInput();
+  // hint event code //
+  document.getElementById('name').addEventListener('mouseover', mouseOver);
+  document.getElementById('name').addEventListener('mouseout', mouseOut);
+  
+  function mouseOver() {
+    document.getElementById('name').innerHTML = 'Ronnie Raymond';
+  }
 
-document.getElementById('hint').addEventListener('mouseover', mouseOver);
-document.getElementById('hint').addEventListener('mouseout', mouseOut);
-
-function mouseOver () {
-    document.getElementById('hint').innerHTML = 'there are more than you think';
-}
-function mouseOut(){
-  document.getElementById('hint').innerHTML = '';
-}
-
-const answers = document.getElementById('answers');
-answers.addEventListener('click', function() {
-  alert('Ronnie Raymond/Martin Stein, Mikhail Arkadan/Jason Thomas, Ronnie Raymond/Jason Thomas, Ronnie/Stein/Arkadan, Jason Thomas/Martin Stein, Jason Thomas/Firehawk, Fire Elemental');
+  function mouseOut(){
+    document.getElementById('name').innerHTML = 'Name (hover)';
+  }
+  
+  // answer event code //
+  const answers = document.getElementById('answers');
+  answers.addEventListener('click', function() {
+    alert('DeathStorm');
+  })
 })
